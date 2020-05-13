1ad316ff5b3f82614feeb705bdc94a63a53d35e7
Zaheer Abbas <zaheer1247@gmail.com>
2020-05-13 14:48:54 +0530

    add /zaheer endpoint

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
 
+app.get('/zaheer', function (req, res) {
+  res.send('Hello Zaheer!\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
